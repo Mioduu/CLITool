5c5bc72cabfa875ad59a5d0a73780a7fb61cbbd8
Mioduu <macieos360x@gmail.com>
2025-07-04 23:20:45 +0200

    Poprawka graficzna i loop

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"sync"
 	"time"
 
 	"github.com/gen2brain/beeep"
@@ -13,61 +11,57 @@ func main() {
 	fmt.Println("Program do trackowania pracy i dobrych nawyków")
 	startTimer := time.Now()
 
-	var wg sync.WaitGroup
-	wg.Add(3)
-
 	positionInterval := time.Minute * 1
 	waterInterval := time.Minute * 1
 	walkInterval := time.Minute * 1
 
 	go func() {
-		defer wg.Done()
 		start := time.Now()
 		for {
 			elapsed := time.Since(start)
 			remaining := waterInterval - elapsed
 			if remaining <= 0 {
 				beeep.Alert("Nawadniaj się", "Napij się wody ;D", "")
-				break
+				start = time.Now()
+			} else {
+				fmt.Printf("\n=== [Woda] ===\nPozostało: %v\n", remaining.Round(time.Second))
+				time.Sleep(time.Second * 5)
 			}
-			fmt.Printf("[Woda] Pozostało: %v\n", remaining.Round(time.Second))
-			time.Sleep(time.Second * 5)
 		}
 	}()
 
 	go func() {
-		defer wg.Done()
 		start := time.Now()
 		for {
 			elapsed := time.Since(start)
 			remaining := positionInterval - elapsed
 			if remaining <= 0 {
 				beeep.Alert("Baczność!", "Wyprostuj się ;)", "")
-				break
+				start = time.Now()
+			} else {
+				fmt.Printf("\n=== [Pozycja] ===\nPozostało: %v\n", remaining.Round(time.Second))
+				time.Sleep(time.Second * 5)
 			}
-			fmt.Printf("[Pozycja] Pozostało: %v\n", remaining.Round(time.Second))
-			time.Sleep(time.Second * 5)
 		}
 	}()
 
 	go func() {
-		defer wg.Done()
 		start := time.Now()
 		for {
 			elapsed := time.Since(start)
 			remaining := walkInterval - elapsed
 			if remaining <= 0 {
 				beeep.Alert("Czas na spacer", "Wyjdź dotknij trawe ;p", "")
-				break
+				start = time.Now()
+			} else {
+				fmt.Printf("\n=== [Spacer] ===\nPozostało: %v\n", remaining.Round(time.Second))
+				time.Sleep(time.Second * 5)
 			}
-			fmt.Printf("[Spacer] Pozostało: %v\n", remaining.Round(time.Second))
-			time.Sleep(time.Second * 5)
 		}
 	}()
 
-	wg.Wait()
-
-	endTimer := time.Since(startTimer)
-	fmt.Printf("Pracujesz od: %v minut \n", endTimer.Round(time.Minute))
-	os.Exit(1)
+	for {
+		time.Sleep(time.Minute)
+		fmt.Printf("Pracujesz od: %v minut \n", time.Since(startTimer).Round(time.Minute))
+	}
 }
